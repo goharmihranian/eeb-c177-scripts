c14822fa6160a500e18278bd79c894b2778a6cab
goharmihranian <mihraniangohar@gmail.com>
2020-02-03 22:24:12 -0800

    part 1 homework 5

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -4,5 +4,5 @@
 # change delimiter to spaces
 #sort according to the 6th (numeric) column in descending order
 #redirected to a file
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ';' -f 2-6 | tr ";" " " | sort -r -n -k 6 > bodym.csv
+tail -n +2 $1 | cut -d ';' -f 2-6 | tr ";" " " | sort -r -n -k 6 > $2
 
